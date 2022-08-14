680bb43d6d1f739268f70718e5f3d7c8c62fe397
Kajetan Domagala <kajtekd8@gmail.com>
2022-08-14 13:05:52 +0200

    build: initial commit
    added and configured typescript and scss

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img src="./assets/markdown.svg" class="logo" alt="logo" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #e4f808d6);
+}
+</style>
