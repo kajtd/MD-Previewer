54fc821e41400e25113e2c34149d0c7549b2665d
Kajetan Domagala <kajtekd8@gmail.com>
2022-08-23 20:24:08 +0200

    build: add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
 import './style.scss';
 import App from './App.vue';
+import { createPinia } from 'pinia';
 
-createApp(App).mount('#app');
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia).mount('#app');
