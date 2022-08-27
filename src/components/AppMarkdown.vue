fb161b832aa4d8d6393f206131a476796247455c
Kajetan Domagala <kajtekd8@gmail.com>
2022-08-27 20:55:50 +0200

    feat: add styles for markdown preview

--- src/components/AppMarkdown.vue
+++ src/components/AppMarkdown.vue
@@ -78,5 +78,109 @@ watchEffect(() => {
     display: block;
     color: $gray-400;
   }
+
+  &__preview {
+    @include mono-text;
+    gap: 1.25em;
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      font-family: $font-slab;
+      font-weight: bold;
+      color: $gray-100;
+    }
+    h1 {
+      font-size: 2em;
+    }
+    h2 {
+      font-size: 1.75em;
+      font-weight: lighter;
+    }
+    h3 {
+      font-size: 1.5em;
+    }
+    h4 {
+      font-size: 1.25em;
+    }
+    h5 {
+      font-size: 1em;
+    }
+    h6 {
+      font-size: 0.875em;
+    }
+
+    p {
+      @include slab-text;
+      color: $gray-400;
+      code {
+        @include mono-text;
+        color: $gray-500;
+      }
+    }
+
+    ol,
+    ul {
+      @include slab-text;
+      padding-left: 1.5em;
+      color: $gray-400;
+    }
+    ul {
+      list-style: none;
+      display: flex;
+      flex-direction: column;
+      gap: 0.625em;
+      li {
+        padding-left: 1.5em;
+        position: relative;
+        line-height: 1.5em;
+        &::before {
+          content: '';
+          position: absolute;
+          left: 0;
+          top: 8px;
+          color: $primary;
+          background: $primary;
+          width: 4px;
+          height: 4px;
+          border-radius: 50%;
+        }
+      }
+    }
+
+    blockquote {
+      background-color: $gray-800;
+      padding: 1.5em;
+      border-radius: 0.25em;
+      border-left: 0.25em solid $primary;
+      margin-left: 0;
+      margin-top: 2em;
+      p,
+      a {
+        font-weight: bold;
+        font-size: 1em;
+        line-height: 1.5em;
+        color: $gray-100;
+      }
+      a:hover {
+        transition: color 0.3s ease-in-out;
+        color: $primary;
+      }
+    }
+    pre {
+      background-color: $gray-800;
+      padding: 1.5em;
+      border-radius: 0.25em;
+      display: block;
+      white-space: pre-wrap;
+      code {
+        @include mono-text;
+        color: $gray-100;
+      }
+    }
+  }
 }
 </style>
